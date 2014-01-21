c97d6bf832e9daf53e2736795e198a801720716c
Clarence <clarenceboey@gmail.com>
2014-01-20 19:04:09 -0500

    fix

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,9 +3,6 @@ class StoreController < ApplicationController
   	@categories = Category.all
   	@products = Product.all
 
-  	    admin = User.find[0]
-  AdminUser.find[0] = admin
-  x =AdminUser.find[0]
-x
+  	
   end
 end
